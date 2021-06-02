8e1cc048714189f88d2f69bcf74a48499043d5cc
i.svirid <i.svirid@a1qa.com>
2021-06-02 20:35:04 +0300

    Add project files.

---
+++ test123/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace test123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long i = 5;
+            object o = i;
+            long l = (long)o;
+            Console.Write(l);
+            Console.ReadKey();
+        }
+    }
+}
